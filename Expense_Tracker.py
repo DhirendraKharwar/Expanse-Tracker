82a4de631929e5750f8bade6ee7f6cdd7efb209a
Dhirendra Kharwar <60399899+DhirendraKharwar@users.noreply.github.com>
2025-03-31 13:16:47 +0530

    Add files via upload

---
+++ Expense_Tracker.py
@@ -0,0 +1,132 @@
+'''
+Add expense
+    1. input the data from the user
+    2. store it in the dictionary
+    3. print successful added massege
+'''
+def add_expense(expenses):
+    date = input("Enter Date YYYY-MM-DD: ")
+    category= input("Enter the Category: ")
+    amount = float(input("Enter the expense amount: "))
+    description= input("Enter the description: ")
+    
+    exp = {'date':date,'category':category,'amount':amount,'description':description}
+    expenses.append(exp)
+    print("Recorded successfully.")
+    
+'''
+View Expense Record
+    1. If record is not found give the messege
+    2. print the records from the dictionary
+'''
+def view_expense(expenses):
+    if not expenses:
+        print("No record found.\n Thank You!")
+    print("  Date               Category               Amount               Description")
+    print("-"*50)
+    for exp in expenses:
+        print(f"  {exp['date']}               {exp['category']}               {exp['amount']:.2f}               {exp['description']}")
+    print("-"*50)
+    
+'''
+Set the Budget
+'''
+def set_budget():
+    return float(input("Enter your monthly budget: "))
+
+'''
+Track the expense recods and budget
+'''
+def track_budget(expenses, budget):
+    total_spent = sum(exp['amount'] for exp in expenses)
+    print(f"Total Spent: Rs. {total_spent:.2f}")
+    if total_spent>budget:
+        print("Warning: You have exceeded budget!")
+        print(f"Exceeded Amount: Rs. {total_spent-budget:.2f}")
+    else:
+        print(f"Remaining Budget: Rs. {budget-total_spent:.2f}")
+
+
+'''
+Call all the functions here to navigate all the activity in this expese tracker
+'''
+def main():
+    expenses = []
+    budget = set_budget()
+    while True:
+        print("Epense Tracker is here")
+        print("1. Add expense records.")
+        print("2. View expense records.")
+        print("3. Track budget.")
+        print("4. Exit")
+        
+        choise = int(input("Enter your choise: "))
+        
+        if choise==1:
+            add_expense(expenses)
+        elif choise==2:
+            view_expense(expenses)
+        elif choise==3:
+            track_budget(expenses,budget)
+        elif choise==4:
+            print("Exit done.")
+            exit()
+        else:
+            print("Please correct your choise, try again.")
+        
+if __name__=="__main__":
+    main()
+
+#Output
+'''
+Enter your monthly budget: 200
+Epense Tracker is here  
+1. Add expense records. 
+2. View expense records.
+3. Track budget.        
+4. Exit
+Enter your choise: 1    
+Enter Date YYYY-MM-DD: 2023-04-09
+Enter the Category: Clothes
+Enter the expense amount: 150
+Enter the description: Jeans
+Recorded successfully.
+Epense Tracker is here
+1. Add expense records.
+2. View expense records.
+3. Track budget.
+4. Exit
+Enter your choise: 1
+Enter Date YYYY-MM-DD: 2025-03-31
+Enter the Category: Eating Out
+Enter the expense amount: 250
+Enter the description: Momos
+Recorded successfully.
+Epense Tracker is here
+1. Add expense records.
+2. View expense records.
+3. Track budget.
+4. Exit
+Enter your choise: 2
+  Date               Category               Amount               Description
+--------------------------------------------------
+  2023-04-09               Clothes               150.00               Jeans
+  2025-03-31               Eating Out               250.00               Momos
+--------------------------------------------------
+Epense Tracker is here
+1. Add expense records.
+2. View expense records.
+3. Track budget.
+4. Exit
+Enter your choise: 3
+Total Spent: Rs. 400.00
+Warning: You have exceeded budget!
+Exceeded Amount: Rs. 200.00
+Epense Tracker is here
+1. Add expense records.
+2. View expense records.
+3. Track budget.
+4. Exit
+Enter your choise: 4
+Exit done.
+'''
